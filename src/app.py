2ab127097d1c37fee8e313adb8a377aa3afc02ca
MichalinaJanik <mi@mft0energy.com>
2023-11-09 19:19:32 +0100

    FIXING INDEXING FOR THE ELEMENTS

--- src/app.py
+++ src/app.py
@@ -75,11 +75,10 @@ def get_className_state(full_className, className_to_check):
     name_list = full_className.strip().split()
     return className_to_check in name_list
 
-def generate_checklist_li_id(id):
-    return {"type": "list_item", "id": id}
 
-def generate_checklist_ul_id(id):
-    return {"type": "unordered_list", "id": id}
+def generate_checklist_ul_id(path):
+    return {"type": "unordered_list", "id": "_".join(path)}
+
 
 def get_path_to_id_in_serialized_ul(ul: dict, obj_id: str, _upstream_path=None):
     """
@@ -116,11 +115,11 @@ def get_path_to_id_in_serialized_ul(ul: dict, obj_id: str, _upstream_path=None):
 def get_by_path(obj, path):
     return reduce(operator.getitem, path, obj)
 
-def get_leaves_below_sidebar_obj(ul_children: dict, path_to_obj: Union[str, tuple, list]):
+def get_leaves_below_sidebar_obj(ul_children: dict, path_to_obj: Union[str, tuple, list] = None):
     """
-    Get all leaf nodes (Li objects without corresponding Ul) from a dict defining the the children of a Ul
+    Get all leaf nodes (Li objects without corresponding Ul) from a dict defining the children of a Ul
     Args:
-        ul_children: Children attribute of a Ul object, defined as a dictionary.  Same format as Dash passes to a
+        ul_children: Children attribute of a Ul object, defined as a dictionary. Same format as Dash passes to a
                      callback watching the "children" attribute of a Ul.
         path_to_obj: A tuple of keys and indices defining where to start within ul_children when looking for children.
                      For example:
@@ -130,57 +129,40 @@ def get_leaves_below_sidebar_obj(ul_children: dict, path_to_obj: Union[str, tupl
         (list): List of references to the leaves found in ul_children (if mutated in place, they will change the
                 ul_children instance)
     """
-    # Traverse the children of this object, returning and child Li objs that have no paired Ul or any leaves of nested
-    # Uls
-    if path_to_obj:
-        if not isinstance(path_to_obj, (tuple, list)):
-            path_to_obj = (path_to_obj,)
+    # Traverse the children of this object, returning any child Li objs that have no paired Ul or any leaves of nested Uls
+    if path_to_obj is None:
+        path_to_obj = ()
 
-        start_obj = get_by_path(ul_children, path_to_obj)
-    else:
-        start_obj = ul_children
+    if not isinstance(path_to_obj, (tuple, list)):
+        path_to_obj = (path_to_obj,)
 
-    to_return = []
+    # Helper function to check if an object is a leaf Li
+    def is_leaf(obj):
+        return obj['type'] == 'Li'
 
-    # Temp variables to determine which items are leaves
-    lis = {}
-    uls_ids = set()
+    def traverse(obj, current_path):
+        to_return = []
 
-    # start_obj may be pointed at a dict defining a dash component with format:
-    #   {
-    #       'namespace': ...,
-    #       'type': ...,
-    #       'props': {
-    #           'children': ...,
-    #       }
-    #   }
-    # Where we want to inspect the 'children' to see if there are any Li's without Ul's.  Or, we might be pointed
-    # directly at 'children' (such as when getting input directly from a dash callback input).  Figure out which
-    # situation we're in
-    if 'props' in start_obj:
-        path_to_obj = path_to_obj + ('props', 'children')
-        start_obj = start_obj['props']['children']
+        for i, child in enumerate(obj):
+            child_path = current_path + (i,)
 
-    # If we have children, recurse.  Else, return this
-    if isinstance(start_obj, (tuple, list)):
-        for i, child in enumerate(start_obj):
-            child_type = child['type']
-            child_idname = child['props']['id']['id']
-            if child_type == 'Li':
-                lis[child_idname] = child
-            elif child_type == 'Ul':
-                uls_ids.add(child_idname)
-                to_return.extend(get_leaves_below_sidebar_obj(ul_children, path_to_obj + (i,)))
-    else:
-        to_return.append(start_obj)
+            # Check if the child is a leaf Li
+            if is_leaf(child):
+                to_return.append(child)
+            else:
+                # If there are children, go deeper
+                if 'props' in child and 'children' in child['props']:
+                    to_return.extend(traverse(child['props']['children'], child_path))
 
-    # Add Lis found that have no paired Uls
-    to_add_to_return = [li for li_id, li in lis.items() if li_id not in uls_ids]
-    to_return.extend(to_add_to_return)
+        return to_return
 
-    return to_return
+    return traverse(ul_children, path_to_obj)
 
-def make_sidebar_children(data, top_item, inherited_class="", child_class=""):
+
+def generate_checklist_li_id(path):
+    return {"type": "list_item", "id": "_".join(path)}
+
+def make_sidebar_children(data, top_item, inherited_class="", child_class="", path=None):
     """
     Recursively generate a hierarchical list defined by data, starting at top_item, using Ul and Li objects
     Ul and Li objects ids are defined by Dash id dicts so they can be subscribed to by a callback as a group
@@ -200,27 +182,32 @@ def make_sidebar_children(data, top_item, inherited_class="", child_class=""):
         inherited_class (str): HTML class name to apply once to all levels of the list
         child_class (str): HTML class name to apply once per step in the list (so Item 1-1 would have it once,
                            Item 1-1-1 would have it twice, etc.).  Useful for incrementing tab behaviour
+        path (list): List representing the path to the current element in the hierarchy
     Returns:
         (list): List of html elements for use as the children attribute of a html.Ul
     """
     this_className = f"{inherited_class} {child_class}"
     content = []
 
+    path = path or []
+
     for name in data[top_item]:
+        leaf_id = generate_checklist_li_id(path + [name])
         content.append(html.Li(
             children=name,
-            id=generate_checklist_li_id(name),
+            id=leaf_id,
             className=this_className,
         ))
 
         if name in data:
-            nested_children = make_sidebar_children(data, name, inherited_class=this_className, child_class=child_class)
+            nested_children = make_sidebar_children(data, name, inherited_class=this_className, child_class=child_class, path=path + [name])
             content.append(html.Ul(
-                id=generate_checklist_ul_id(name),
+                id=generate_checklist_ul_id(path + [name]),
                 children=nested_children,
             ))
 
     return content
+
 
 def make_sidebar_ul(data, top_item, inherited_class="", child_class=""):
     """
