e9ab66ea6ef96e26265ea22e934a9a9e6e4ff4a0
MichalinaJanik <mi@mft0energy.com>
2023-11-12 12:34:57 +0100

    Adding the state dictionary

--- src/data/data.py
+++ src/data/data.py
@@ -210,6 +210,21 @@ energy_categories_types = [
 
 category_trees = [energy_activities, energy_categories_types]
 
+states_dict = {'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas',
+               'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado',
+               'CT': 'Connecticut', 'DC': 'District of Columbia',
+               'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii',
+               'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana',
+               'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts',
+               'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri',
+               'MS': 'Mississippi', 'MT': 'Montana', 'NC': 'North Carolina', 'ND': 'North Dakota',
+               'NE': 'Nebraska', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NV': 'Nevada',
+               'NY': 'New York', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania',
+               'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee',
+               'TX': 'Texas', 'US': 'United States', 'UT': 'Utah', 'VA': 'Virginia', 'VT': 'Vermont',
+               'WA': 'Washington', 'WI': 'Wisconsin', 'WV': 'West Virginia', 'WY': 'Wyoming',
+               'X3': 'Federal Offshore, Gulf of Mexico', 'X5': 'Federa Offshore, Pacific'}
+
 
 # For derived values or values that need to be calculated from the data
 def some_calculation(param1, param2):
